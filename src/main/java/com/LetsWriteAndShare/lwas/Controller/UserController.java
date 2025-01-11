9bd5e4b5cb5aa0c693e3b7c47be519c371547e8f
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-01-11 22:55:51 +0300

    ApiErrors

--- src/main/java/com/LetsWriteAndShare/lwas/Controller/UserController.java
+++ src/main/java/com/LetsWriteAndShare/lwas/Controller/UserController.java
@@ -2,11 +2,16 @@ package com.LetsWriteAndShare.lwas.Controller;
 
 
 import com.LetsWriteAndShare.lwas.entity.User;
+import com.LetsWriteAndShare.lwas.errors.ApiErrors;
 import com.LetsWriteAndShare.lwas.service.UserService;
 import com.LetsWriteAndShare.lwas.utils.GenericMessage;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UserController {
@@ -22,11 +27,23 @@ public class UserController {
     }
 
     @PostMapping("/api/v1/users")
-    GenericMessage createUser(@RequestBody User user) {
+        ResponseEntity<?>  createUser(@RequestBody User user) {
+    if (user.getUsername() == null || user.getUsername().isEmpty()){
+        ApiErrors apiErrors = new ApiErrors();
+        apiErrors.setMessage("Validation Errors");
+        apiErrors.setPath("/api/v1/users");
+        apiErrors.setStatus(400);
+        Map<String,String> validationErrors = new HashMap<>();
+        validationErrors.put("username", "username can not be null");
+        apiErrors.setValidationErrors(validationErrors);
+
+
+
+        return ResponseEntity.badRequest().body(apiErrors);
+        }
 
         userService.save(user);
-
-        return new GenericMessage("user is created!!");
+        return ResponseEntity.ok( new GenericMessage("user is created!!"));
     }
 
 
