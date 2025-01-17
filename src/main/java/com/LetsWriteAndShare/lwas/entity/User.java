e3b48596676e5b538dc5b263f7cbfff6c1e3abc8
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-01-18 02:59:35 +0300

    UniqueConstraint

--- src/main/java/com/LetsWriteAndShare/lwas/entity/User.java
+++ src/main/java/com/LetsWriteAndShare/lwas/entity/User.java
@@ -1,17 +1,14 @@
 package com.LetsWriteAndShare.lwas.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name = "users")
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = {"email"}))
 public class User {
 
 
