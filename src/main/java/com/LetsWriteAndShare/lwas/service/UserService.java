3868f6476f3803590ba9d9a1fe8333bb05d4e407
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-01-20 17:50:37 +0300

    E-mail chech and JavaMailSender added

--- src/main/java/com/LetsWriteAndShare/lwas/service/UserService.java
+++ src/main/java/com/LetsWriteAndShare/lwas/service/UserService.java
@@ -3,11 +3,16 @@ package com.LetsWriteAndShare.lwas.service;
 import com.LetsWriteAndShare.lwas.Exception.NotUniqueEmailException;
 import com.LetsWriteAndShare.lwas.Repository.UserRepository;
 import com.LetsWriteAndShare.lwas.entity.User;
+import org.apache.logging.log4j.message.SimpleMessage;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Properties;
 import java.util.UUID;
 
 
@@ -34,10 +39,52 @@ public class UserService {
             user.setActivationToken(UUID.randomUUID().toString());
             //  user.setPassword(passwordEncoder.encode(user.getPassword()));
             userRepository.save(user);
-
+            sendActivationEmail(user);
         } catch (DataIntegrityViolationException ex) {
             throw new NotUniqueEmailException();
         }
 
     }
+
+    private void sendActivationEmail(User user) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("furkan@this-app.com");
+        message.setTo(user.getEmail());
+        message.setSubject("Account Activation");
+        message.setText("http://localhost:5173/activaiton/" + user.getActivationToken());
+
+        getJavaMailSender().send(message);
+
+
+    }
+        public JavaMailSender getJavaMailSender(){
+            JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+            mailSender.setHost("smtp.ethereal.email");
+            mailSender.setPort(	587);
+            mailSender.setUsername("abdul.howe28@ethereal.email");
+            mailSender.setPassword("74pT5AdyKZDqjxeE6v");
+
+
+            Properties properties = mailSender.getJavaMailProperties();
+            properties.put("mail.smtp.starttls.enable" , "true");
+            return mailSender;
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
