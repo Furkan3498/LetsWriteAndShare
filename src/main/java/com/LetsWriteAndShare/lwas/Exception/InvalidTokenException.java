4a45cee84805b4dd0e6ce5ee5163cebdca22de99
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-01-25 18:36:50 +0300

    activateUser

--- src/main/java/com/LetsWriteAndShare/lwas/Exception/InvalidTokenException.java
+++ src/main/java/com/LetsWriteAndShare/lwas/Exception/InvalidTokenException.java
@@ -1,4 +1,11 @@
 package com.LetsWriteAndShare.lwas.Exception;
 
+import com.LetsWriteAndShare.lwas.utils.Messages;
+import org.springframework.context.i18n.LocaleContextHolder;
+
 public class InvalidTokenException extends RuntimeException{
+
+    public  InvalidTokenException(){
+        super(Messages.getMessageForLocale("LetsWriteAndShare.activate.user.invalid.token", LocaleContextHolder.getLocale()));
+    }
 }
