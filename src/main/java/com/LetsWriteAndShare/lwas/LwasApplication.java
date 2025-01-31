9ec2775963b016973c9b8dcd0cb5fc7c2c4513a5
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-01-31 20:06:59 +0300

    CommandLineRunner

--- src/main/java/com/LetsWriteAndShare/lwas/LwasApplication.java
+++ src/main/java/com/LetsWriteAndShare/lwas/LwasApplication.java
@@ -1,14 +1,44 @@
 package com.LetsWriteAndShare.lwas;
 
+import com.LetsWriteAndShare.lwas.Repository.UserRepository;
+import com.LetsWriteAndShare.lwas.entity.User;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class LwasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LwasApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LwasApplication.class, args);
+    }
 
+
+    @Bean
+	@Profile("dev")
+    CommandLineRunner userCreator(UserRepository userRepository) {
+
+
+		return  new CommandLineRunner() {
+
+			PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+			@Override
+			public void run(String... args) throws Exception {
+					for (var i= 1; i<=25 ;i++){
+						User user = new User();
+						user.setUsername("user" + 1);
+						user.setEmail("user"+i+"@gmail.com");
+						user.setPassword(passwordEncoder.encode("P4ssword"));
+						user.setActive(true);
+						userRepository.save(user);
+					}
+			}
+		};
+
+    }
 }
