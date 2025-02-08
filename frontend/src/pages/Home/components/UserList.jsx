ede3fca187c9530dc51ff29453bfbd5d8646cc53
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-02-08 22:58:16 +0300

    page

--- frontend/src/pages/Home/components/UserList.jsx
+++ frontend/src/pages/Home/components/UserList.jsx
@@ -1,17 +1,26 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { loadUser } from "./api";
+import { Button } from "bootstrap";
 
 export function UserList(){
-    const [users, setUsers] = useState([]);
+    const [userPage, setUserPage] = useState({
+            content:[],
+            last: false,
+            first : false,
+            number : 0
 
+    });
+
+
+        const getUsers = useCallback(async (page) => {
+
+            const response = await loadUser(page);
+            setUserPage(response.data);
+            
+        }, [])
 
     useEffect(() => {
-        async function getUsers() {
-
-            const response = await loadUser();
-            setUsers(response.data);
-            
-        }
+        
         getUsers();
 
     }, []);
@@ -19,10 +28,13 @@ export function UserList(){
     return(
         <>
         <div> User List </div>
-        {   users.map((user) =>{
+        {   userPage.content.map((user) =>{
             return    <div>{user.username}</div>;
             })
         }
+
+       { !userPage.first &&<button onClick={() => getUsers(userPage.number - 1)}>Previous</button>}
+       { !userPage.last &&<button onClick={() => getUsers(userPage.number + 1)}>Next</button>}
         
         
         </>
