0b0310895a5da66b66d3733b971b33ea56bbdfa3
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-02-26 02:49:04 +0300

    REFACTOR ErrorHandler

--- src/main/java/com/LetsWriteAndShare/lwas/errors/ErrorHandler.java
+++ src/main/java/com/LetsWriteAndShare/lwas/errors/ErrorHandler.java
@@ -15,85 +15,52 @@ import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ErrorHandler {
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ExceptionHandler({MethodArgumentNotValidException.class,
+            NotUniqueEmailException.class,
+            ActivationNotificationException.class,
+            InvalidTokenException.class,
+            NotFoundException.class,
+            AuthenticationException.class})
         //@ResponseStatus(HttpStatus.BAD_REQUEST)
         //ResponseEntity yerine böyle de kullanabilirdim.
-    ResponseEntity<ApiErrors> handleMethodArgNotValidEx(MethodArgumentNotValidException exception  ,HttpServletRequest request) {
-        ApiErrors apiErrors = new ApiErrors();
-        apiErrors.setPath(request.getRequestURI());
-        String message = Messages.getMessageForLocale("LetsWriteAndShare.error.validation",  LocaleContextHolder.getLocale());
-
-        apiErrors.setMessage(message);
-
-        apiErrors.setStatus(400);
-        // Map<String, String> validationErrors = new HashMap<>();
-        //for( var fieldError : exception.getBindingResult().getFieldErrors()){
-        //  validationErrors.put(fieldError.getField(),fieldError.getDefaultMessage()); }
-
-        var validationErrors = exception.getBindingResult().getFieldErrors().stream().collect(Collectors.toMap(
-                FieldError::getField, FieldError::getDefaultMessage, (existing, replacing) -> existing));
-        apiErrors.setValidationErrors(validationErrors);
-        return ResponseEntity.badRequest().body(apiErrors);
-
-
-    }
-    @ExceptionHandler(NotUniqueEmailException.class)
-
-    ResponseEntity<ApiErrors> handleNotUniqueEmailEx(NotUniqueEmailException exception) {
-        ApiErrors apiErrors = new ApiErrors();
-
-        apiErrors.setPath("/api/v1/users");
-
-        apiErrors.setMessage(exception.getMessage());
-        apiErrors.setStatus(400);
-
-        apiErrors.setValidationErrors(exception.getValidErrors());
-        return ResponseEntity.status(400).body(apiErrors);
-
-
-    }
-    @ExceptionHandler(ActivationNotificationException.class)
-
-    public ResponseEntity<ApiErrors> handleActivationNotificationException(ActivationNotificationException exception) {
-        ApiErrors apiErrors = new ApiErrors();
-
-        apiErrors.setPath("/api/v1/users");
-
-        apiErrors.setMessage(exception.getMessage());
-        apiErrors.setStatus(502);
-
-        return ResponseEntity.status(502).body(apiErrors);
-
-
-    }
-    @ExceptionHandler(InvalidTokenException.class)
-    public ResponseEntity<ApiErrors> handleInvalidTokenException(InvalidTokenException exception, HttpServletRequest request) {
+    ResponseEntity<ApiErrors> handleException(Exception exception  ,HttpServletRequest request) {
         ApiErrors apiErrors = new ApiErrors();
         apiErrors.setPath(request.getRequestURI());
         apiErrors.setMessage(exception.getMessage());
-        apiErrors.setStatus(400);
 
-        return ResponseEntity.status(400).body(apiErrors);
+        if (exception instanceof MethodArgumentNotValidException){
+            String message = Messages.getMessageForLocale("LetsWriteAndShare.error.validation",  LocaleContextHolder.getLocale());
+
+            apiErrors.setMessage(message);
+
+            apiErrors.setStatus(400);
+            // Map<String, String> validationErrors = new HashMap<>();
+            //for( var fieldError : exception.getBindingResult().getFieldErrors()){
+            //  validationErrors.put(fieldError.getField(),fieldError.getDefaultMessage()); }
+
+            var validationErrors = ((MethodArgumentNotValidException)exception).getBindingResult().getFieldErrors().stream().collect(Collectors.toMap(
+                    FieldError::getField, FieldError::getDefaultMessage, (existing, replacing) -> existing));
+            apiErrors.setValidationErrors(validationErrors);
+        } else if (exception instanceof  NotUniqueEmailException) {
+            apiErrors.setStatus(400);
+            apiErrors.setValidationErrors(((NotUniqueEmailException)exception).getValidErrors());
+            
+        } else if (exception instanceof  ActivationNotificationException) {
+            apiErrors.setStatus(502);
+            
+        } else if (exception instanceof  InvalidTokenException) {
+            apiErrors.setStatus(400);
+
+        }else if (exception instanceof  NotFoundException) {
+            apiErrors.setStatus(404);
+
+        }else if (exception instanceof  AuthenticationException) {
+            apiErrors.setStatus(401);
+
+        }
+        return ResponseEntity.status(apiErrors.getStatus()).body(apiErrors);
 
 
     }
-    @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ApiErrors> handleEntityNotFoundException(NotFoundException exception , HttpServletRequest request) {
-        ApiErrors apiErrors = new ApiErrors();
-        apiErrors.setPath(request.getRequestURI());
-        apiErrors.setMessage(exception.getMessage());
-        apiErrors.setStatus(404);
 
-        return ResponseEntity.status(404).body(apiErrors);
-
-
-    }
-    @ExceptionHandler(AuthenticationException.class)
-    ResponseEntity<?>  handleAuthenticationException(AuthenticationException exception){
-        ApiErrors errors = new ApiErrors();
-        errors.setPath("/api/v1/auth");
-        errors.setStatus(401);
-        errors.setMessage(exception.getMessage());
-        return ResponseEntity.status(401).body(errors);
-    }
 }
