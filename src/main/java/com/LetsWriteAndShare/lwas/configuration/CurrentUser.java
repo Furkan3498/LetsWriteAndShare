f31c4c9464e6f5917ac9f7726a2ba2db72a6dc7d
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-03-15 17:51:55 +0300

    CurrentUser

---
+++ src/main/java/com/LetsWriteAndShare/lwas/configuration/CurrentUser.java
@@ -0,0 +1,73 @@
+package com.LetsWriteAndShare.lwas.configuration;
+
+import com.LetsWriteAndShare.lwas.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class CurrentUser implements UserDetails {
+
+    long id;
+    String username;
+    String password;
+
+
+    public CurrentUser(User user){
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+    }
+
+    @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return AuthorityUtils.createAuthorityList("ROLE_USER");
+        }
+
+        @Override
+        public String getPassword() {
+            return this.password;
+        }
+
+        @Override
+        public String getUsername() {
+            return  this.username;
+
+
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
+    public void setUsername(String usernane) {
+        this.username = usernane;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+};
+
--- src/main/java/com/LetsWriteAndShare/lwas/service/AppUserDetailService.java
+++ src/main/java/com/LetsWriteAndShare/lwas/service/AppUserDetailService.java
@@ -1,6 +1,7 @@
 package com.LetsWriteAndShare.lwas.service;
 
 
+import com.LetsWriteAndShare.lwas.configuration.CurrentUser;
 import com.LetsWriteAndShare.lwas.entity.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -27,40 +28,5 @@ public class AppUserDetailService implements UserDetailsService {
         if (inDb == null){
             throw  new UsernameNotFoundException(email +" is not found");
         }
-        return new UserDetails() {
-            @Override
-            public Collection<? extends GrantedAuthority> getAuthorities() {
-                return AuthorityUtils.createAuthorityList("ROLE_USER");
-            }
-
-            @Override
-            public String getPassword() {
-                return inDb.getPassword();
-            }
-
-            @Override
-            public String getUsername() {
-                return inDb.getUsername();
-
-
-            }
-
-            @Override
-            public boolean isAccountNonExpired() {
-                return true;
-            }
-            @Override
-            public boolean isAccountNonLocked() {
-                return true;
-            }
-            @Override
-            public boolean isCredentialsNonExpired() {
-                return true;
-            }
-            @Override
-            public boolean isEnabled() {
-                return true;
-            }
-        };
-    }
-}
+        return  new CurrentUser(inDb);
+    }}
