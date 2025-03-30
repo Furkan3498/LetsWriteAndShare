e68d3dd36e92eb36c8710616ab8df0928bf37e25
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-03-30 03:45:51 +0300

    FileServicesaveBase64StringAsFilee

--- src/main/java/com/LetsWriteAndShare/lwas/file/FileService.java
+++ src/main/java/com/LetsWriteAndShare/lwas/file/FileService.java
@@ -3,6 +3,27 @@ package com.LetsWriteAndShare.lwas.file;
 
 import org.springframework.stereotype.Service;
 
+import java.io.*;
+import java.util.Base64;
+import java.util.UUID;
+
 @Service
 public class FileService {
+    public String saveBase64StringAsFile(String image) {
+
+        String fileName = UUID.randomUUID().toString();
+
+        File file =new File(fileName);
+
+        try {
+            OutputStream outputStream = new FileOutputStream(file);
+            byte[] base64Decoded = Base64.getDecoder().decode(image);
+            outputStream.write(base64Decoded);
+            outputStream.close();
+            return fileName;
+        } catch (IOException e) {
+           e.printStackTrace();;
+        }
+        return null;
+    }
 }
