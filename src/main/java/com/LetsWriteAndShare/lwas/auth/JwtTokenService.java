994e654b313cddb1ff0f1180761ef7df4f85e53c
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-04-12 20:22:39 +0300

    createToken with JWT

--- src/main/java/com/LetsWriteAndShare/lwas/auth/JwtTokenService.java
+++ src/main/java/com/LetsWriteAndShare/lwas/auth/JwtTokenService.java
@@ -3,8 +3,13 @@ package com.LetsWriteAndShare.lwas.auth;
 import com.LetsWriteAndShare.lwas.dto.Credentials;
 import com.LetsWriteAndShare.lwas.entity.User;
 import com.LetsWriteAndShare.lwas.service.TokenService;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
 
 
 @Service
@@ -12,7 +17,10 @@ import org.springframework.stereotype.Service;
 public class JwtTokenService implements TokenService {
     @Override
     public Token createToken(User user, Credentials credentials) {
-        return null;
+        SecretKey key = Keys.hmacShaKeyFor("secret-must-be-at-least-32-chars".getBytes());
+        String token = Jwts.builder().setSubject(Long.toString(user.getId())).signWith(key).compact();
+        return new Token("Bearer",token);
+
     }
 
     @Override
