2957a738c292de94971e0aa1d9ef41c313b91db5
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-04-12 20:27:44 +0300

    TokenFilter

---
+++ src/main/java/com/LetsWriteAndShare/lwas/configuration/TokenFilter.java
@@ -0,0 +1,25 @@
+package com.LetsWriteAndShare.lwas.configuration;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+
+@Component
+public class TokenFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+
+        System.err.println("-------");
+        System.err.println("filter is running");
+        System.err.println("-------");
+
+        filterChain.doFilter(request,response);
+    }
+}
