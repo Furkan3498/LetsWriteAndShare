3de4b0ac24a1a196fd3828ae582c6199d16c804b
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-04-12 20:52:55 +0300

    doFilterInternal

--- src/main/java/com/LetsWriteAndShare/lwas/configuration/TokenFilter.java
+++ src/main/java/com/LetsWriteAndShare/lwas/configuration/TokenFilter.java
@@ -6,6 +6,9 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -28,9 +31,14 @@ public class TokenFilter extends OncePerRequestFilter {
       String authorizationHeader = request.getHeader("Authorization");
       if (authorizationHeader !=null){
           User user = tokenService.verifyToken(authorizationHeader);
+
+          if (user != null){
+              CurrentUser currentUser = new CurrentUser(user);
+              UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(currentUser,
+                      null,currentUser.getAuthorities());
+
+          }
       }
-
-
         filterChain.doFilter(request,response);
     }
 }
