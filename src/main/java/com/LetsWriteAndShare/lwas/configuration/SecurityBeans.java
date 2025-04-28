135e849a2b92e326f41035addce42c0f49a6f12d
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-04-28 23:55:20 +0300

    SecurityBeans

--- src/main/java/com/LetsWriteAndShare/lwas/configuration/SecurityBeans.java
+++ src/main/java/com/LetsWriteAndShare/lwas/configuration/SecurityBeans.java
@@ -1,8 +1,16 @@
 package com.LetsWriteAndShare.lwas.configuration;
 
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SecurityBeans {
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
