cdcf7fa65ed6570a5f39022f9d06f2c16603cabd
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-04-29 21:52:09 +0300

    trying to make my own token

--- src/main/java/com/LetsWriteAndShare/lwas/auth/Token.java
+++ src/main/java/com/LetsWriteAndShare/lwas/auth/Token.java
@@ -1,8 +1,37 @@
 package com.LetsWriteAndShare.lwas.auth;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Token{
 
-    String prefix ;
+    String prefix = "Bearer";
+
+    @Id
     String token;
 
+    public Token(String prefix, String token) {
+        this.prefix = prefix;
+        this.token = token;
+    }
+
+    public Token() {
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
