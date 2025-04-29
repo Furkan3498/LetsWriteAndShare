8e8bdcc64e2298cf2f63ec67b3f4511b8b3d8ddd
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-04-29 22:01:40 +0300

    trying to make my own token

--- src/main/java/com/LetsWriteAndShare/lwas/auth/OpaqueTokenService.java
+++ src/main/java/com/LetsWriteAndShare/lwas/auth/OpaqueTokenService.java
@@ -7,9 +7,11 @@ import com.LetsWriteAndShare.lwas.service.TokenService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 
 @Service
-@ConditionalOnProperty(name = "lwas.token-type" , havingValue = "basic")
+@ConditionalOnProperty(name = "lwas.token-type" , havingValue = "opaque")
 public class OpaqueTokenService implements TokenService {
 
     private final TokenRepository tokenRepository;
@@ -20,7 +22,10 @@ public class OpaqueTokenService implements TokenService {
 
     @Override
     public Token createToken(User user, Credentials credentials) {
-        return null;
+       String randomValue = UUID.randomUUID().toString();
+       Token token = new Token();
+       token.setToken(randomValue);
+       return tokenRepository.save(token);
     }
 
     @Override
